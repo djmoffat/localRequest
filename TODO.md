6ed3b3a44022aa339ab328d97a78d30fa4caefca
Dave Moffat <me@davemoffat.com>
2018-04-30 14:33:00 +0100

    implementing timeout functinality

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO
 
-* Timeout - with previous request timestamp, and required wait process
 * Path - to stored pickle file
 * ForceUpdate - to force a renew of all downloaded files
 * ForceLocal - to force no new post requests to be posted, so return an error or None file
--- localRequests.py
+++ localRequests.py
@@ -2,34 +2,47 @@
 
 import requests
 import pickle
+import time
+import pdb
 
+timestamp = time.time()
 
-def get(url,params=None):
+def get(url,params=None,timeout=0):
 	dirpath = '/Volumes/Internal/Documents/localRequest/data/'
 	filePath = dirpath + url.replace('/','__') + '.pkl'
 	if params is not None:
 		payload = makeParamPayload(params)
+	else:
+		payload = None
 	try:
 		fn = open(filePath,'r') 
 		data = pickle.load(fn)
+		timestamp = data['timestamp']
 		fn.close()
 		if payload not in data:
-			data[payload] = postRequests(url,params)
-			fn = open(filePath,'a') 
+			data[payload] = postRequests(url,params,timeout)
+			timestamp = time.time()
+			data['timestamp'] = timestamp
+			fn = open(filePath,'w') 
 			pickle.dump(data,fn)
 			fn.close()
 		return data[payload]
 	except IOError: 
 		print 'Error: File does not appear to exist.'
 		data = dict()
-		data[payload] = postRequests(url,params)
+		data[payload] = postRequests(url,params,timeout)
+		timestamp = time.time()
+		data['timestamp'] = timestamp
 		fn = open(filePath,'w+') 
 		pickle.dump(data,fn)
 		fn.close()
 	return data[payload]
 
-def postRequests(url,params):
-	print 'posting request'
+def postRequests(url,params,timeout):
+	# print 'posting request'
+	waitTime = time.time() - timestamp
+	if waitTime <= timeout:
+		time.sleep(timeout-waitTime)
 	data = requests.get(url, params = params)
 	return {'content':data.content,'url':data.url,'headers':data.headers,'status_code':data.status_code,'reason':data.reason}
 	
