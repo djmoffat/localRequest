fda4594846fe07592640bd307827fda7b0eac14e
Dave Moffat <me@davemoffat.com>
2018-04-30 14:46:51 +0100

    implementing source parameter, for local-force local request or remote-force remote request and update

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 # TODO
 
 * Path - to stored pickle file
-* ForceUpdate - to force a renew of all downloaded files
-* ForceLocal - to force no new post requests to be posted, so return an error or None file
+* Test force local and force remote parameters
--- localRequests.py
+++ localRequests.py
@@ -7,7 +7,17 @@ import pdb
 
 timestamp = time.time()
 
-def get(url,params=None,timeout=0):
+def get(url,params=None,timeout=0,source=None):
+	'''
+	* url is the classic post url
+	* params are the standard parameters to post to a url, and used as the key in the storage dict.
+	* timeout, in seconds, will take a timestamp record at the previous post requests, and if necessary, perform a wait function 
+	to ensure the specificed time has elapsed.
+	* source options: None, local, remote
+	 ** None (default): will look up local files unless it does not exist, where a post request will be made and saved
+	 ** local: will force only use of local files, and return None type for a parameter option not used before
+	 ** remote: will force a pull and update of all local files, overwriting each one
+	'''
 	dirpath = '/Volumes/Internal/Documents/localRequest/data/'
 	filePath = dirpath + url.replace('/','__') + '.pkl'
 	if params is not None:
@@ -22,23 +32,30 @@ def get(url,params=None,timeout=0):
 		else:
 			data['timestamp'] = timestamp
 		fn.close()
-		if payload not in data:
+		if source == 'local':
+			if payload not in data:
+				return None
+		else:
+			if payload not in data or source == 'remote':
+				data[payload] = postRequests(url,params,timeout)
+				timestamp = time.time()
+				data['timestamp'] = timestamp
+				fn = open(filePath,'w') 
+				pickle.dump(data,fn)
+				fn.close()
+		return data[payload]
+	except IOError: 
+		print 'File does not appear to exist - Creating File'
+		if source == 'local':
+			print 'Cannot create new file and populate where source = local'
+		else:
+			data = dict()
 			data[payload] = postRequests(url,params,timeout)
 			timestamp = time.time()
 			data['timestamp'] = timestamp
-			fn = open(filePath,'w') 
+			fn = open(filePath,'w+') 
 			pickle.dump(data,fn)
 			fn.close()
-		return data[payload]
-	except IOError: 
-		print 'Error: File does not appear to exist.'
-		data = dict()
-		data[payload] = postRequests(url,params,timeout)
-		timestamp = time.time()
-		data['timestamp'] = timestamp
-		fn = open(filePath,'w+') 
-		pickle.dump(data,fn)
-		fn.close()
 	return data[payload]
 
 def postRequests(url,params,timeout):
